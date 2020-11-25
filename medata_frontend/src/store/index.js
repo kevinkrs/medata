d7ba5b6598ecf0c601133a6cbbf06d7fdd2d6c86
ubptj <kevin.kraus@student.kit.edu>
2020-11-25 14:41:35 +0100

    initial structure upload

---
+++ medata_frontend/src/store/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const state = {
+  // single source of data
+}
+
+const actions = {
+  // asynchronous operations
+}
+
+const mutations = {
+  // isolated data mutations
+}
+
+const getters = {
+  // reusable data accessors
+}
+
+const store = new Vuex.Store({
+  state,
+  actions,
+  mutations,
+  getters
+})
+
+export default store ({
+
+})
