120ee57319f9758fa223cdf23bf8d282af7b4f69
uvuyo <jan.bode@student.kit.edu>
2020-12-04 10:09:17 +0100

    upadte create_mock_data

--- medata_backend/create_mock_data.py
+++ medata_backend/create_mock_data.py
@@ -1,17 +1,21 @@
 from app import db, Information, Insights, Categories
 import random
-insight_names = ["number_inputs", "recall", "number_outputs", "accuracy", "f2 Score", "color", 
-"height", "gender", "foo", "bar", "moin", "Bode", "Krauss", "Heydemann", "Effenberger", "K-Town", "IISM"]
 
-categories_names = ["laboratory experiments", "supervised learning by classification", "category3"]
 
-for x in range(len(insight_names))
-    i1=Insights(id=x, name=insight_names[x])
-    c3=Categories(insightId=x, name = random.choice(categories_names))
-    inf1 =Information(insightId=x, paperId=543+x, answer1=f"first answer: {random()}", answer1_upvotes=random.randint(2,13), insight_upvotes=random.randint(1,14))
 
-    db.session.add(i1)
-    db.session.add(c3)
-    db.session.add(inf1)
+ def create_mock_dataa():
+    insight_names = ["number_inputs", "recall", "number_outputs", "accuracy", "f2 Score", "color", 
+    "height", "gender", "foo", "bar", "moin", "Bode", "Krauss", "Heydemann", "Effenberger", "K-Town", "IISM"]
 
-db.session.commit()
+    categories_names = ["laboratory experiments", "supervised learning by classification", "category3"]
+    db.create_all()
+    for x in range(len(insight_names))
+        i1=Insights(id=x, name=insight_names[x])
+        c3=Categories(insightId=x, name = random.choice(categories_names))
+        inf1 =Information(insightId=x, paperId=543+x, answer1=f"first answer: {random()}", answer1_upvotes=random.randint(2,13), insight_upvotes=random.randint(1,14))
+
+        db.session.add(i1)
+        db.session.add(c3)
+        db.session.add(inf1)
+
+    db.session.commit()
