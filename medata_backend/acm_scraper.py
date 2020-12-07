a3c23f7790fffc8ed9f1083cfdc81cc7e51d0b1a
Jan Effenberger <jan@effenberger-ol.de>
2020-12-07 13:53:23 +0100

    Name gets pulled from authors profile

--- medata_backend/acm_scraper.py
+++ medata_backend/acm_scraper.py
@@ -17,8 +17,22 @@ organizational_chart = soup.find("ol", class_="rlist organizational-chart")
 print(title)
 print(f"there are {len(authors_info)} authors in this paper")
 
+
+'''
+finds the name of the author on his/her unique profile. quite handy if we want to display more detailed data later on.
+'''
+def nameFromProfile(link):
+    url = "https://dl.acm.org"+link
+    html = requests.get(url).text
+    profile = BeautifulSoup(html, "lxml")
+
+    name = profile.find(class_="colored-block item-meta profile-meta").find("h2").text.replace("  "," ")
+    return name
+    
+    
 for author in authors_info:
     #print(f"{author}\n\n\n")
     print(author.find("a").get("title")) #Name of the Author
     print(author.find(class_="author-info__body").find("p").text) #Institute he/she is working at
     print(author.find(class_="author-info").find("a").get("href")) #link to their profile
+    print(nameFromProfile(author.find(class_="author-info").find("a").get("href")))      
