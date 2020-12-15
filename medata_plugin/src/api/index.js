cafe0e62ce0a9f41a1830fb29d0e89651751adae
ubptj <kevin.kraus@student.kit.edu>
2020-12-15 09:40:03 +0100

    new api methods

--- medata_plugin/src/api/index.js
+++ medata_plugin/src/api/index.js
@@ -1,50 +1,46 @@
 /* eslint-disable no-template-curly-in-string */
 import axios from 'axios'
 
-/*
-// Mock Data
-const metadata = [{
-    id: 1,
-    name: 'Accuracy',
-    value: '84%',
-    upvotes: 0 // not confirmed = red
-  },
-  {
-    id: 2,
-    name: 'Recall',
-    value: '91%',
-    upvotes: 3 // validation needed = yellow
-  },
-  {
-    id: 3,
-    name: 'F1',
-    value: '87%',
-    upvotes: 10 // confirmed = green
-  }]
-  
-  export function fetchMetadata () {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => { resolve(metadata) }, 300)
-    })
-  }*/
-  
 
 
-
-// API URL from backend
-const API_URL = 'http://127.0.0.1:5000/'
+// Query by user is send to backend
+axios.defaults.baseURL = 'http://127.0.0.1:5000/'
 
 // Query by user is send to backend
 export function fetchMetadata () {
-  return axios.get(`${API_URL}/get_specific`)
-}
-
-// Function for the user interaction with existing inightsexport function saveResponse (questionResponse) {
-export function setResponse () {  
-return axios.put(`${API_URL}/metadata/${questionResponse.id}`, questionResponse)
+  return axios.get(`${'get_specific'}`)
 }
 
 // Function for the userInput of new relevant metadata
-export function postUserInput (userInput) {
-  return axios.post(`${API_URL}/metadata/`, userInput)
+export function postInsight (inPaperId, inInsight, inCategories) {
+  return axios.post(`${'add_insight'}`, {
+    paper_id: inPaperId,
+    insight: inInsight,
+    categories: inCategories
+  })
+}
+
+export function postAnswer (inPaperId, inInsight, inAnswer) {
+  return axios.post(`${'add_answer'}`, {
+    paper_id: inPaperId,
+    insight: inInsight,
+    answer: inAnswer
+  })
+}
+
+export function postRateAnswer (inPaperId, inInsight, inAnswer, inUpvote) {
+  return axios.post(`${'rate_answer'}`, {
+    paper_id: inPaperId,
+    insight: inInsight,
+    answer: inAnswer,
+    upvote: inUpvote
+  })
+}
+
+export function postRateRelevanceInsight (inPaperId, inInsight, inUpvote) {
+  return axios.post(`${'rate_relevance_insight'}`, {
+    paper_id: inPaperId,
+    insight: inInsight,
+    upvote: inUpvote
+  })
 }
