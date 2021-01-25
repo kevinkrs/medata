30db204890bae53acc148896d54f90037cc501eb
Jan Bode <uvuyo@student.kit.edu>
2021-01-25 15:27:48 +0100

    restructuring

---
+++ medata_backend/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example-pkg-YOUR-USERNAME-HERE", # Replace with your own username
+    version="1.0",
+    author="Kevin Kraus, Jan Effenberger, Max Heydemann, Merlin Knäble, Tim Rietz",
+    author_email="uvuyo@student.kit.edu",
+    description="Backend for Medata",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
