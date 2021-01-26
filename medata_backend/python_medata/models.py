1a88343ca30633d3eefdae9804a597e829ba48c7
Jan Bode <uvuyo@student.kit.edu>
2021-01-26 19:19:38 +0100

    models pycco

--- medata_backend/python_medata/models.py
+++ medata_backend/python_medata/models.py
@@ -1,29 +1,30 @@
 """ 
-Backend documentation:
+** Backend documentation: **
 
 1. [[__init__.py]]
 2. [[acm_scraper.py]]
 3. [[api.py]]
 4. [[app.py]]
 5. [[create_init_data.py]]
+6. [[models.py]]
 """
 
 """
-**models.py** 
-this module contains the model for our database
+** models.py ** 
 
-variables like insight, categories etc. are always refered to with: ''
+* this module contains the model for our database
+* variables like insight, categories etc. are always refered to with: ' '
+* e.g. we have one 'insight' called "accuracy" 
+* this 'insight' supports the 'category' "supervised learning by classification"
+* on all acm papers in the 'category' "supervised learning by classification" exists one 'information' related to the 'insight'
+* all those 'information' can have multiple answers (because the value for accuracy is not the same in all papers)
 
-e.g. we have one 'insight' called "accuracy" 
-this 'insight' supports the 'category' "supervised learning by classification"
-on all acm papers in the 'category' "supervised learning by classification" exists one 'information' related to the 'insight'
-all those 'information' can have multiple answers (because the value for accuracy is not the same in all papers)
+** Classes: **
 
-classes:
-1. 
-2.
-3. 
-4. 
+1. Insights 
+2. Categories 
+3. Information
+4. Answers
 
 """
 
@@ -36,15 +37,16 @@ from sqlalchemy.orm import backref
 
 db = SQLAlchemy()
 
-# === Insights Model ===
+# === Insights ===
 class Insights(db.Model):
     """ 
-    Explanation:
-        An 'insight' object has it's unique name
-        Typo errors (e.g. spelling mistakes) are counted in typo_error
-        'insight' has a one-to-many relationship with 'categories' -> one insight is supported for multiple 'categories'
-        'insight' has a one-to-many relationship with 'information' -> 'information' saves additional data for a specific acm-paper (see 'information')
-        The to_dict method is very powerful and returns basically the whole db, used only for testing
+        ** Explanation: **
+
+        * an 'insight' object has it's unique name
+        * typo errors (e.g. spelling mistakes) are counted in typo_error
+        * 'insight' has a one-to-many relationship with 'categories' -> one insight is supported for multiple 'categories'
+        * 'insight' has a one-to-many relationship with 'information' -> 'information' saves additional data for a specific acm-paper (see 'information')
+        * the to_dict method is very powerful and returns basically the whole db, used only for testing
 
     """
     __tablename__ = 'insights'  
@@ -59,9 +61,9 @@ class Insights(db.Model):
 
     def to_dict(self):
         """ 
+            ** Returns: **
 
-        Returns:
-            json: returns name and all linked 'information' (with 'answers') and 'categories'
+            * json: returns name and all linked 'information' (with 'answers') and 'categories'
         """
         return dict(id = self.id,
         name = self.name,
@@ -72,16 +74,17 @@ class Insights(db.Model):
     def __repr__(self):
         return f'id: {self.id}, name: {self.name}'
 
-
+# === Categories ===
 class Categories(db.Model):
-    """ Category Model
+    """ 
 
-    Explanation:
-        'categories' keep track of the supported categories for a specific 'insight'
-        'categories' are linked to an insight via insight_id
-        name stores the name of the category
-        downvote_category indicates the relevance of an 'insight' for a specific 'category' (the less downvotes, the better)
-        to_dict is used only for testing
+        **Explanation:**
+
+        * 'categories' keep track of the supported categories for a specific 'insight'
+        * 'categories' are linked to an insight via insight_id
+        * name stores the name of the category
+        * downvote_category indicates the relevance of an 'insight' for a specific 'category' (the less downvotes, the better)
+        * to_dict is used only for testing
 
     """
     __tablename__ = 'categories'
@@ -91,34 +94,28 @@ class Categories(db.Model):
     name = db.Column(db.String(30))
     downvote_category = db.Column(db.Integer, default = 0)
     timestamp = db.Column(db.DateTime, default = datetime.utcnow)
-
     
     def to_dict(self):
-        """ Category name to a dict
-
-        Returns:
-            dict: name of the category as a string
-        """
         return dict(category_name = self.name)
 
     def __repr__(self):
          return f'category_id: {self.category_id}, insight_id: {self.insight_id}, name: {self.name}, downvote_category: {self.downvote_category}, timestamp: {self.timestamp} '
 
-
+# === Information ===
 class Information(db.Model):
-    """ Information Model
+    """ 
+    
+        **Explanation:**
 
-    Explanation:
-        An 'information' is a more specific representation of an 'insight', which saves additional data related to one paper from acm
-        insight_id is determined by a foreign key, it links the 'information' with it's 'insight'
-        insight_name is doubled but necessary. Otherwise we run into problems as two foreign keys are used
-        paper_id is the link to the paper stored as a String
-        insight_up/downvotes is selfexplanatory
-        title, authors, authors_profile_link and conference of the acm-paper are saved in our db
-        'information' has a one-to-many relationship with 'answers'
+        * an 'information' is a more specific representation of an 'insight', which saves additional data related to one paper from acm
+        * insight_id is determined by a foreign key, it links the 'information' with it's 'insight'
+        * insight_name is doubled but necessary - otherwise we run into problems as two foreign keys are used
+        * paper_id is the link to the paper stored as a String
+        * insight_up/downvotes is selfexplanatory
+        * title, authors, authors_profile_link and conference of the acm-paper are saved in our db
+        * 'information' has a one-to-many relationship with 'answers'
 
     """
-
     __tablename__ = 'information'
 
     information_id = db.Column(db.Integer, primary_key=True)
@@ -139,10 +136,11 @@ class Information(db.Model):
     def to_dict(self):
         """ 
 
-        Answers are limited by limit_answers()
+            * answers are limited by limit_answers()
 
-        Returns:
-            json: all relevant columns of 'information' and the for most relevant 'answers'
+            **Returns:**
+
+            * json: all relevant columns of 'information' and the for most relevant 'answers'
         """
         return dict(id = self.insight_id,
         name = self.insight_name, 
@@ -157,10 +155,13 @@ class Information(db.Model):
         )
 
     def limit_answers(self):
-        """ limit the returned 'answers' to answer_limit
+        """ 
+        
+            * limit the returned 'answers' to answer_limit
 
-        Returns:
-            list: of answer.to_dict() ranked by descending answer score, if two 'answers' have the same score, the newer one is preferred 
+            **Returns:**
+
+            * list: of answer.to_dict() ranked by descending answer score, if two 'answers' have the same score, the newer one is preferred 
         """
         answer_limit = 4
         limited_answers = Answers.query.filter(Answers.information_id==self.information_id).order_by(Answers.answer_score.desc(), Answers.timestamp.desc()).limit(answer_limit).all()
@@ -169,15 +170,16 @@ class Information(db.Model):
     def __repr__(self):
         return f'insight_id: {self.insight_id}, information_id: {self.information_id}, paper_id: {self.paper_id}, authors: {self.authors}'
 
-
+# === Answers ===
 class Answers(db.Model):
-    """ Answers Model
+    """ 
 
-    Explanation:
-        information_id links the 'answer' to one 'information'
-        'answer' is the actual answer to the 'insight'/'information' asked for in the paper, stored as a string
-        answer_up/downvotes selfexplanatory
-        answer_score makes it easier to rank the 'answers'
+        *Explanation:
+
+        * information_id links the 'answer' to one 'information'
+        * 'answer' is the actual answer to the 'insight'/'information' asked for in the paper, stored as a string
+        * answer_up/downvotes selfexplanatory
+        * answer_score makes it easier to rank the 'answers'
     """
     __tablename__ = 'answers'
 
@@ -190,10 +192,10 @@ class Answers(db.Model):
     timestamp = db.Column(db.DateTime, default = datetime.utcnow)
 
     def to_dict(self):
-        """ Answer as a dict
+        """ 
 
-        Returns:
-            dict: all information stored in this object
+            **Returns:**
+            * dict: all information stored in this object
         """
         return dict(
         answer = self.answer,
