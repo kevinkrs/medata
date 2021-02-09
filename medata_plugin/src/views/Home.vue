4d54233a34fa2083be30be107b7038814fb1e8ba
ubptj <kevin.kraus@student.kit.edu>
2021-02-09 10:35:40 +0100

    bugfix frontend

--- medata_plugin/src/views/Home.vue
+++ medata_plugin/src/views/Home.vue
@@ -661,7 +661,7 @@ export default {
     border-radius: 5px 0px 0px 5px;
   }
   .insight-name-transparent {
-    width: 349%;
+    width: 343%;
     padding: 8px;
     color: transparent;
     margin-top: -8px;
